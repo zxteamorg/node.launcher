59dfa120175590c94b23c947c83fd1073ace6b90
Max Anurin <maxim.anurin@zxteam.org>
2019-10-04 02:03:32 +0300

    Add package version conflict guard. Export IoC decorators.

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,42 @@
+const { name: packageName, version: packageVersion } = require("../package.json");
+const G: any = global || window || {};
+const PACKAGE_GUARD: symbol = Symbol.for(packageName);
+if (PACKAGE_GUARD in G) {
+	const conflictVersion = G[PACKAGE_GUARD];
+	// tslint:disable-next-line: max-line-length
+	const msg = `Conflict module version. Look like two different version of package ${packageName} was loaded inside the process: ${conflictVersion} and ${packageVersion}.`;
+	if (process !== undefined && process.env !== undefined && process.env.NODE_ALLOW_CONFLICT_MODULES === "1") {
+		console.warn(msg + " This treats as warning because NODE_ALLOW_CONFLICT_MODULES is set.");
+	} else {
+		throw new Error(msg + " Use NODE_ALLOW_CONFLICT_MODULES=\"1\" to treats this error as warning.");
+	}
+} else {
+	G[PACKAGE_GUARD] = packageVersion;
+}
+
 import { Configuration } from "@zxteam/contract";
-import * as cfg from "@zxteam/configuration";
+import { fileConfiguration } from "@zxteam/configuration";
+import { logger } from "@zxteam/logger";
 
 import * as fs from "fs";
 import * as util from "util";
 
+import { Provided as ProvidedOrig } from "typescript-ioc";
+
+/*
+	This module provides a facade for IoC functionality.
+	Currently we use https://www.npmjs.com/package/typescript-ioc
+		but we may change IoC library in future.
+	[!] The application should not import "typescript-ioc" directly
+*/
+export { AutoWired, Container, Inject, Provides, Singleton } from "typescript-ioc";
+export function Provided<T>(provider: Provider<T>): (target: Function) => void {
+	return ProvidedOrig(provider);
+}
+export interface Provider<T> { get(): T; }
+
 const readFile = util.promisify(fs.readFile);
 
-import loggerFactory from "@zxteam/logger";
 
 export interface Runtime {
 	destroy(): Promise<void>;
@@ -20,7 +50,7 @@ export function launcher<T>(runtimeFactory: RuntimeFactory<T>): void;
 export function launcher<T>(configurationFactory: ConfigurationFactory<T>, runtimeFactory: RuntimeFactory<T>): void;
 
 export function launcher<T>(...args: Array<any>): void {
-	const log = loggerFactory.getLogger("launcher");
+	const log = logger.getLogger("launcher");
 
 	async function run() {
 		process.on("unhandledRejection", reason => {
@@ -103,7 +133,7 @@ export function fileConfigurationFactory<T>(parser: (configuration: Configuratio
 		const configFileArg = process.argv.find(w => w.startsWith("--config="));
 		if (configFileArg !== undefined) {
 			const configFile = configFileArg.substring(9); // Cut --config=
-			return parser(cfg.fileConfiguration(configFile));
+			return parser(fileConfiguration(configFile));
 		}
 		throw new LaunchError("An argument --config is not passed");
 	});
